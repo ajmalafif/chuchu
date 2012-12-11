132f5b59834630a8f860aae77a02babf942dc590
Ajmal Afif <ajmalafif@gmail.com>
2012-12-12 05:22:48 +0800

    tweaked gitignore to ignore sensitive files

--- config/initializers/instagram.rb
+++
@@ -1,9 +0,0 @@
-require "instagram"
-
-Instagram.configure do |config|
-
-  config.client_id = "b5814935692442a7b44f6be8d63aadea"
-
-  config.access_token = "9339750.b581493.b08380ade80a4b19bba6d6b67c8c06b9"
-
-end
