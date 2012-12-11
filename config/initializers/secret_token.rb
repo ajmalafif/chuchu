7e9e97a105b7f5d14f357309cc054d6d2fac9a27
Ajmal Afif <ajmalafif@gmail.com>
2012-12-12 03:58:58 +0800

    added Rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Chacha::Application.config.secret_token = 'cf0a384df67f5553d47eb4101d97b0ab5ce9ee67eaf9fc1766ecfa64c779cce373b9c7aa3c75bdca65e6bd82d395820a250d024104db4a1cf387b241f6ea8175'
